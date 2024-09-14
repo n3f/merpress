3c6d5c84e608f9608fe61ab31f4a46f1b0637b46
brent <b@n3f.co>
2024-09-14 10:48:35 -0700

    Add support for align block control.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { registerBlockType } from '@wordpress/blocks';
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-// import './style.scss';
+import './style.scss';
 
 /**
  * Internal dependencies
