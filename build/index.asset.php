7c354ef0e6587fa334f50fa7ff493819a68d5e6b
brent <b@n3f.co>
2024-10-11 16:29:45 -0700

    Mermaid 11.3.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,5 @@
 <?php declare( strict_types=1 );
 return array(
 	'dependencies' => array( 'react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-notices', 'wp-primitives' ),
-	'version' => '8120d023c66c219b7fe0',
+	'version' => 'ba2a39f4f3527262f99a',
 );
